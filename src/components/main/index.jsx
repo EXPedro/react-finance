fb82394678fd7ae66d7d6beee9e44fbc6cbdab57
EXPedro <pedroelisio555@gmail.com>
2022-03-05 23:09:39 -0300

    Create Padlock component

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import './Main.css';
+import Padlock from "../padlock";
 
 const Main = () => {
   return(
     <>
-      <h2>main</h2>
+    <div className='main'>
+      <Padlock />
+    </div>  
     </>
   )
 };
---
+++ src/components/padlock/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Padlock.css';
+
+const Padlock = () => {
+  return(
+    <>
+    <div className='padlock'>
+      <h2>padlock</h2>
+    </div>  
+    </>
+  )
+};
+
+export default Padlock;
