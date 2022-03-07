7246080889be08d8c566d4eb449135f23a455b55
EXPedro <pedroelisio555@gmail.com>
2022-03-07 15:03:57 -0300

    Merge some card grid areas

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -4,15 +4,13 @@ import './Card.css';
 const Card = () => {
   return(
     <>
-      <div className="card">
-        <div className="grid-card no-margins">
-          <p className="ticker no-margins">ITSA4</p>
-          <p className="quantidade no-margins">351</p>
-          <p className="pm no-margins">R$ </p>
-          <p className="atual no-margins">R$ </p>
-          <p className="endline no-margins">12.03 %</p>
-        </div>
-      </div>    
+      <div className="card">        
+        <p className="ticker no-margins">ITSA4</p>
+        <p className="quantidade no-margins">351</p>
+        <p className="pm no-margins">R$ </p>
+        <p className="atual no-margins">R$ </p>
+        <p className="endline no-margins">12.03 %</p>
+      </div>
     </>
   )
 };
